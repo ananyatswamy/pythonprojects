e39bf149058933195c53f55c13b3e4f78abd3d2b
Ananya <ananyatswamy@gmail.com>
2024-12-08 18:50:11 -0500

    validate ip works now

--- the_internet.py
+++ the_internet.py
@@ -10,10 +10,12 @@ Description:
 
 def create_server(internet_server, user_server_name, user_ip):
     # see if ip is valid
+    validate_ip(user_ip)
     # see if ip is existing in dictionary (internet_server,user_ip)
     # if exists return error message
     # elif server name exists overwrite ip
     # else add to dictionary
+
 
 def create_connection(internet_server, server1, server2, connection_time):
     # if server 1 and server 2 exist in dictionary
@@ -26,7 +28,7 @@ def create_connection(internet_server, server1, server2, connection_time):
 
     # if not connected add server name and connection time to both servers
     # print ( connection exists )
-
+    pass
 def set_server(internet_server, name_or_ip):
     #if first digit is numerical then it is ip & is valid ip
         # ip_exist ( true )
@@ -34,7 +36,7 @@ def set_server(internet_server, name_or_ip):
       # print not valid server name
       # false ret
     # set the server ip/server name
-
+    pass
 
 def ping(internet_server, target_server):
     # identify if it is ip or name
@@ -42,21 +44,21 @@ def ping(internet_server, target_server):
     # find name if it is ip
     # from current server identify if there is connection to target server
     # return connection_time + recursivecall()
-
+    pass
 def traceout(internet_server, target_server):
     # identify if it is ip or name
     # identify if it exists
     # find name if it is ip
     # from current server identify if there is connection to target server
     # return connection_time + recursivecall()
-
+    pass
 
 def display_server(internet_server):
     # print all servers their connections and ip addresses
-
+    pass
 def ip_config(internet_server):
      # print() current location
-
+    pass
 
 # helper functions..
 
@@ -65,12 +67,14 @@ def server_exists(internet_server,server):
     # check the server name from the
     # if exists
     # server_valid = True
-    return server_valid;
+    return server_valid
+
 
 def ip_exists(internet_server,user_ip):
     # iterate the dictionary
     #print("Invalid IP address,already exists.")
     # return true or false
+    pass
 
 def validate_ip(ip_address):
     parts = ip_address.split('.')
@@ -93,14 +97,13 @@ if __name__ == "__main__":
     # this will have key as server name
     # values will be ip address, connection1, connection_time1, connection2...
     command = input(">>>").lower().split()
-    while command != ("quit"):
+
+    while command[0] != ("quit"):
         # parsing, command user input
-        if command == "create-server":
-            if len(parts) == 3 and parts[0] == "create-server":
-                domain = command[1]
-                ip_address = command[2]
-                if validate_ip(ip_address):
-                    pass
+        if command[0] == "create-server" and len(command) == 3:
+            name = command[1]
+            ip_address = command[2]
+            create_server(internet_server,name,ip_address)
         elif command == "":
             pass
         elif command == "":
